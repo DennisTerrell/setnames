dfe9b5cde908fb69d6fedf946d1c062c64bb2bb5
Dennis <dennisterrell@gmail.com>
2016-09-26 16:12:55 -0700

    test

--- setnames.sh
+++ setnames.sh
@@ -4,6 +4,6 @@
 echo "Yo $USER, what do you want to name this box?"
 read computername
 
-sudo scutil --set ComputerName "newname"
-sudo scutil --set LocalHostName "newname"
-sudo scutil --set HostName "newname"
+sudo scutil --set ComputerName "$newname"
+sudo scutil --set LocalHostName "$newname"
+sudo scutil --set HostName "$newname"
